82741d9ef5cfd253c88c929d59dfcd65dc2fdfbe
EC2 Default User <ec2-user@ip-172-31-41-184.ap-northeast-2.compute.internal>
2020-11-13 01:14:05 +0000

    region: us-east-1

--- second-account/bin/second-account.ts
+++ second-account/bin/second-account.ts
@@ -3,6 +3,6 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { SecondAccountStack } from '../lib/second-account-stack';
 
-const envRegion = { region: 'us-east-2' };
+const envRegion = { region: 'us-east-1' };
 const app = new cdk.App();
 new SecondAccountStack(app, 'SecondAccountStack', { env: envRegion });
